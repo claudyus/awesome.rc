dad7e3e2a4d8c7ba11a627b4e6542d845405e64d
Claudio Mignanti <c.mignanti@unidata.it>
2014-07-22 11:22:00 +0200

    submodules + nasa astronomy backgroud

--- nasaBackground.sh
+++ nasaBackground.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
- 
+
 # grabs the nasa image of the day by RSS feed and updates the gnome
 # background. add this to your cron jobs to have this happen daily.  this is,
 # obviously, a hack, that is likely to break at the slightest change of NASA's
 # RSS implementation. yay standards!
 
 #EDITED FOR feh
- 
-rss=`wget -q -O - http://www.nasa.gov/rss/lg_image_of_the_day.rss`
-img_url=`echo $rss | grep -o '<enclosure [^>]*>' | grep -o 'http://[^\"]*'`
+
+img_url=`wget -q -O - http://apod.nasa.gov/apod/astropix.html | grep -o '<a [^>]*>' | grep 'jpg' | cut -d \" -f 2`
+img_url=http://apod.nasa.gov/$img_url
 
 #rss=`wget -q -O - http://apod.nasa.gov/apod.rss`
 #img_url=`echo $rss | grep -o '<description [^>]*>' | grep -o 'http://[^\"]*'`
 
- 
-# this command will overwrite the image if it already exists
+# overwrite the image
 wget -q -O $HOME/.config/awesome/nasa_bg.jpg $img_url
- 
+
 feh --bg-scale $HOME/.config/awesome/nasa_bg.jpg
